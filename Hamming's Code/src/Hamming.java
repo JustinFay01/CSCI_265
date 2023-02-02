cd29638bac82eda79e572797d80737da53c6a1d6
Justin Fay <justin.fay@hope.edu>
2023-02-01 22:40:29 -0500

    trying something new

--- Hamming's Code/src/Hamming.java
+++ Hamming's Code/src/Hamming.java
@@ -16,23 +16,15 @@ public class Hamming {
         }
 
         //Input rest of number
-        hamCode[2] = (byte) (val & 1); //3 Slot Ham 1 Slot Char
-        hamCode[4] = (byte) (val >> 1 & 1); //5 Slot Ham 2 Slot Char
-        hamCode[5] = (byte) (val >> 2 & 1); //6 Slot Ham 3 Slot Char
-        hamCode[6] = (byte) (val >> 3 & 1); //7 Slot Ham 4 Slot Char
-        hamCode[8] = (byte) (val >> 4 & 1); //9 Slot Ham 5 Slot Char
-        hamCode[9] = (byte) (val >> 5 & 1); //10 Slot Ham 6 Slot Char
-        hamCode[10] = (byte) (val >> 6 & 1); //11 Slot Ham 7 Slot Char
+        hamCode[2] = (byte) ((val >> 6) & 1);
+        hamCode[4] = (byte) ((val >> 5) & 1); 
+        hamCode[5] = (byte) ((val >> 4) & 1);
+        hamCode[6] = (byte) ((val >> 3) & 1); 
+        hamCode[8] = (byte) ((val >> 2) & 1);
+        hamCode[9] = (byte) ((val >> 1) & 1); 
+        hamCode[10] = (byte) (val & 1); 
         
 
-        // for (int i = 4; i < hamCode.length; i++) { // Loop through values of char to add to non pairty bit slots
-        //     if (i != 7) {
-        //         int bias = 3;
-        //         if (i > 7)
-        //             bias = 4;
-        //         hamCode[i] = (byte) (val >> (i - bias) & 1);
-        //     }
-        // }
 
         printBits(hamCode);
         System.out.println();
@@ -41,11 +33,18 @@ public class Hamming {
     }
 
     public byte[] setEventPairty(char val, byte[] hamCode) {
-        hamCode[0] = (byte) ((val >> 6 & 1) ^ (val >> 4 & 1) ^ (val >> 3 & 1) ^ (val >> 1 & 1) ^ (val & 1)); // 11, 9, 7, 5, 3
-        hamCode[1] = (byte) ((val >> 7 & 1) ^ (val >> 5 & 1) ^ (val >> 2 & 1)); // 12, 10, 6
-        hamCode[3] = (byte) ((val >> 7 & 1) ^ (val >> 5 & 1) ^ (val >> 2 & 1)); // 12, 7, 6, 5
-        hamCode[7] = (byte) ((val >> 7 & 1) ^ (val >> 6 & 1) ^ (val >> 5 & 1) ^ (val >> 4 & 1)); // 12, 11, 10, 9
-
+        /*
+         * Char Shifts Binary from left to right 
+         */ 
+                            //11               //9             //7             //5
+        hamCode[0] = (byte) ((val >> 6 & 1) ^ (val >> 4 & 1) ^ (val >> 1 & 1) ^ (val & 1));
+                             //11                   //10            //6 
+        hamCode[1] = (byte) ((val >> 6 & 1) ^ (val >> 5 & 1) ^ (val >> 2 & 1)); // 11, 10, 6
+                             //7               // 6            //5
+        hamCode[3] = (byte) ((val >> 3 & 1) ^ (val >> 2 & 1) ^ (val >> 1 & 1)); // 7, 6, 5
+                            //11             //10            //9
+        hamCode[7] = (byte) ((val >> 6 & 1) ^ (val >> 5 & 1) ^ (val >> 4 & 1)); // 11, 10, 9
+        
         return hamCode;
     }
 
@@ -61,7 +60,7 @@ public class Hamming {
 
     public void printBits(byte[] bits) {
         for (int i = 0; i < bits.length; i++)
-            System.out.print(bits[i]);
+            System.out.print(bits[i] + " ");
     }
 
     public static void main(String[] args) {
@@ -70,23 +69,21 @@ public class Hamming {
 
         Hamming ham = new Hamming();
 
-        byte[] bits1 = ham.encode('d', ODDPARITY);
-        byte[] bits2 = ham.encode('d', EVENPARITY);
-        
-        System.out.println("\n\n\n");
+        byte[] M = ham.encode('M', EVENPARITY);
+
         // Test #1: 'A' should be 00100001001 in even parity
-        byte[] A = ham.encode('A', EVENPARITY);
-        System.out.print("'A' in even parity is ");
-        ham.printBits(A);
-        System.out.println();
+        // byte[] A = ham.encode('A', EVENPARITY);
+        // System.out.print("'A' in even parity is ");
+        // ham.printBits(A);
+        // System.out.println();
 
         // Test #2: I should get the character back!
-        char AA = ham.decode(A, EVENPARITY);
-        System.out.println("We should get A back: " + AA);
+        // char AA = ham.decode(A, EVENPARITY);
+        // System.out.println("We should get A back: " + AA);
 
-        // Test #3: Flipping 1 bit should not matter
-        A[5] = (byte) (A[5] ^ 1);
-        AA = ham.decode(A, EVENPARITY);
-        System.out.println("We should get A back: " + AA);
+        // // Test #3: Flipping 1 bit should not matter
+        // A[5] = (byte) (A[5] ^ 1);
+        // AA = ham.decode(A, EVENPARITY);
+        // System.out.println("We should get A back: " + AA);
     }
 }
